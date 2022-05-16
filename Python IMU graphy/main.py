075151fbd374e8d7f1aa26688e72f4e505b5cd45
Frank <frankbsmn@gmail.com>
2022-05-16 14:41:16 +0200

    added the python graphing thing

---
+++ Python IMU graphy/main.py
@@ -0,0 +1,55 @@
+from CreaTeBME import connect
+import matplotlib.pyplot as plt
+import time
+import threading
+import random
+
+# AC:67:B2:38:77:86 WirelessIMU-7786
+# AC:67:B2:38:87:32 WirelessIMU-8732
+
+data = [[] for i in range(6)]
+names = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
+colors = ['red', 'green', 'blue', 'yellow', 'purple', 'black']
+# This just simulates reading from a socket.
+def data_listener():
+    while True:
+        global sensors
+        for sensor in sensors:
+            measurement = sensor.take_measurement()
+            for i, measure in enumerate(measurement):
+                data[i].append(measure)
+                # if len(data[i]) > 50:
+                #     data[i].pop(0)
+
+            # print(measurement)
+            # Do something with the data
+def update_graph():
+    while True:
+        plt.pause(0.001)
+        # data_listener()
+        for i, dataPoint in enumerate(data[::]):
+            axs[0 if i < 3 else 1].plot(dataPoint, color=colors[i])
+            axs[0 if i < 3 else 1].set_xlim([len(dataPoint)-1000, len(dataPoint)])
+
+        # axs[0].legend(names[:3:])
+        # axs[1].legend(names[3::])
+
+        plt.draw()
+
+if __name__ == '__main__':
+    global sensors
+    sensors = connect()
+    thread = threading.Thread(target=data_listener)
+    thread.daemon = True
+    thread.start()
+    #
+    # initialize figure
+    global fig, axs
+    fig, axs = plt.subplots(2)
+
+    # plt.figure()
+    # ln, = plt.plot([])
+    # plt.plot(data)
+    plt.ion()
+    plt.show()
+    update_graph()
---
+++ Python IMU graphy/test 2.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import time
+import threading
+import random
+
+data = []
+
+# This just simulates reading from a socket.
+def data_listener():
+    while True:
+        time.sleep(1)
+        data.append(random.random())
+        print(data)
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=data_listener)
+    thread.daemon = True
+    thread.start()
+    #
+    # initialize figure
+    plt.figure()
+    # ln, = plt.plot([])
+    plt.plot(data)
+    plt.ion()
+    plt.show()
+    while True:
+        plt.pause(1)
+        # data_listener()
+        # ln.set_xdata(range(len(data)))
+        # ln.set_ydata(data)
+        plt.plot(data)
+        plt.draw()
