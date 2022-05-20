f8ee147bbd8a8d56e84ed3d39bc1574cd2868249
ysbrandB <ysbrand@burgstede.eu>
2022-05-20 12:22:26 +0200

    Begin implementing 2 sensors in main2

--- Python IMU graphy/main2.py
+++ Python IMU graphy/main2.py
@@ -10,26 +10,32 @@ from CreaTeBME import connect
 
 frame_rate = 60
 sensors = connect()
-sensor = sensors[0]
-xs = [[] for x in range(6)]
-ys = [[] for x in range(6)]
+
+xs = []
+ys = []
+
+for sensor in sensors:
+    xs.append([[] for x in range(6)])
+    ys.append([[] for x in range(6)])
+
 limit = 500
 counter = 0
-figs, axs = plt.subplots(2)
+figs, axs = plt.subplots(2*len(sensors))
 
 
 def take_measurement():
     while True:
         loop_time = time.perf_counter()
-        global sensor, counter
-        measurement = sensor.take_measurement()
-        for i, measure in enumerate(measurement):
-            xs[i].append(counter)
-            ys[i].append(measure)
-            if len(xs[i]) > limit:
-                xs[i].pop(0)
-            if len(ys[i]) > limit:
-                ys[i].pop(0)
+        global counter
+        for i, sensor in enumerate(sensors):
+            measurement = sensor.take_measurement()
+            for j, measure in enumerate(measurement):
+                xs[i][j].append(counter)
+                ys[i][j].append(measure)
+                if len(xs[i][j]) > limit:
+                    xs[i][j].pop(0)
+                if len(ys[i][j]) > limit:
+                    ys[i][j].pop(0)
         counter += 1
 
 
@@ -40,9 +46,9 @@ def update_graph(counter):
     axs[1].set_ylim([-500, 500])
     axs[0].set_ylim([-5, 5])
     for i in range(3):
-        axs[0].plot(xs[i], ys[i])
+        axs[0].plot(xs[0][i], ys[0][i])
     for i in range(3, 6):
-        axs[1].plot(xs[i], ys[i])
+        axs[1].plot(xs[0][i], ys[0][i])
 
 
 ani = animation.FuncAnimation(figs, update_graph, interval=1/frame_rate)
