9a755df425943569b5073ad8ce4f228b9d55f61c
Leon228x <l.b.migaj@student.utwente.nl>
2022-06-09 14:33:21 +0200

    added the angle calculation.py., not important any more

--- Python IMU graphy/Angler2.py
+++ Python IMU graphy/Angler2.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import threading
 import numpy as np
+from scipy import integrate
 
 from CreaTeBME import connect
 
@@ -23,7 +24,6 @@ limit = 500
 counter = 0
 figs, axs = plt.subplots(3, len(sensors), figsize=(10, 6))
 
-
 def take_measurement():
     while True:
         loop_time = time.perf_counter()
@@ -38,7 +38,7 @@ def take_measurement():
                 if len(ys[i][j]) > limit:
                     ys[i][j].pop(0)
 
-            angle_calc(i, measurement)
+            # angle_calc(i, measurement)
             xs[i][6].append(counter)
             if len(xs[i][6]) > limit:
                 xs[i][6].pop(0)
@@ -47,30 +47,41 @@ def take_measurement():
         counter += 1
 
 
-def angle_calc(sensor_id, measurement):
-    base_x = 0
-    base_y = 0
-    base_z = 1
-    acc_x = measurement[0] - base_x
-    acc_y = measurement[1] - base_y
-    acc_z = measurement[2] - base_z
 
-    initial_x_angle = math.atan(acc_x / math.sqrt(acc_y ** 2 + acc_z ** 2))
-    initial_y_angle = math.atan(acc_y / math.sqrt(acc_x ** 2 + acc_z ** 2))
-
-    average_angle = []
-
-    if len(average_angle) < 120:
-        average_angle.append(initial_x_angle)
-    else:
-        average_angle.pop(0)
-
-    average = 0
-    for i in range(len(average_angle)):
-        average += average_angle[i]
-        average = average/len(average_angle)
-
-    ys[sensor_id][6].append(average)
+# def angle_calc(sensor_id, measurement):
+    # base_x = 0
+    # base_y = 0
+    # base_z = 1
+    #
+    # acc_x = (measurement[0] - base_x)
+    # acc_y = (measurement[1] - base_y)
+    # acc_z = (measurement[2] - base_z)
+    #
+    # angle_x = np.cumsum(measurement[3])/60
+    # angle_x = integrate.cumtrapz(measurement[3], )
+    # angle_y = np.cumsum(measurement[4])/60
+    # angle_z = np.cumsum(measurement[5])/60
+    #
+    #
+    # initial_x_angle = math.atan(acc_x / math.sqrt(acc_y ** 2 + acc_z ** 2))
+    # initial_y_angle = math.atan(acc_y / math.sqrt(acc_x ** 2 + acc_z ** 2))
+    # degrees_x = (180/math.pi) * initial_x_angle
+    # average_angle = []
+    #
+    # if len(average_angle) < 120:
+    #     average_angle.append(initial_x_angle)
+    # else:
+    #     average_angle.pop(0)
+    #
+    # average = 0
+    # for i in range(len(average_angle)):
+    #     average += average_angle[i]
+    #     average = average/len(average_angle)
+    #
+    # ys[sensor_id][6].append(initial_x_angle)
+    #
+    # ys[sensor_id][6].append(angle_x)
+    #
     # print(average)
 
 def update_graph(counter):
@@ -87,16 +98,16 @@ def update_graph(counter):
     else:
         axs[0].clear()
         axs[1].clear()
-        axs[2].clear()
+        # axs[2].clear()
         axs[1].set_ylim([-500, 500])
         axs[0].set_ylim([-5, 5])
-        axs[2].set_ylim([-1, 1])
+        # axs[2].set_ylim([-100, 100])
         for i in range(3):
             axs[0].plot(xs[0][i], ys[0][i])
         for i in range(3, 6):
             axs[1].plot(xs[0][i], ys[0][i])
 
-        axs[2].plot(xs[0][6], ys[0][6])
+        # axs[2].plot(xs[0][6], ys[0][6])
 
 
 ani = animation.FuncAnimation(figs, update_graph, interval=1/frame_rate)
--- Python IMU graphy/angle calculation.py
+++ Python IMU graphy/angle calculation.py
@@ -1,1 +1,95 @@
-print(f"Hello")
+import sys
+import time
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import threading
+import numpy as np
+
+from CreaTeBME import connect
+
+frame_rate = 60
+sensors = connect()
+
+xs = []
+ys = []
+
+for sensor in sensors:
+    xs.append([])
+    ys.append([])
+
+limit = 500
+counter = 0
+figs, axs = plt.subplots(3, len(sensors), figsize=(10, 6))
+
+
+def take_measurement():
+    while True:
+        global counter
+        for i, sensor in enumerate(sensors):
+            xs[i].append(counter)
+            ys[i].append(1)
+
+            if len(xs[i]) > limit:
+                xs[i].pop(0)
+            if len(ys[i]) > limit:
+                ys[i].pop(0)
+        counter += 1
+
+
+            # measurement = sensor.take_measurement()
+            # for j, measure in enumerate(measurement):
+            #     xs[i][j].append(counter)
+            #     ys[i][j].append(measure)
+            #     if len(xs[i][j]) > limit:
+            #         xs[i][j].pop(0)
+            #     if len(ys[i][j]) > limit:
+            #         ys[i][j].pop(0)
+            #
+            #     if j == 3:
+            #         calculate_value(measurement, counter, i)
+
+
+# def calculate_value(gyro_x, counter, sensor_num):
+#     angle = gyro_x
+#
+#     xs[sensor_num][6].append(counter)
+#     ys[sensor_num][6].append(angle)
+#     if len(xs[sensor_num][6]) > limit:
+#         xs[sensor_num][6].pop(0)
+#     if len(ys[sensor_num][6]) > limit:
+#         ys[sensor_num][6].pop(0)
+
+
+def update_graph(counter):
+    if len(sensors) > 1:
+        for i in range (len(sensors)):
+            axs[0][i].clear()
+            axs[1][i].clear()
+            axs[1][i].set_ylim([-500, 500])
+            axs[0][i].set_ylim([-5, 5])
+            for j in range(3):
+                axs[0][i].plot(xs[i][j], ys[i][j])
+            for j in range(3, 6):
+                axs[1][i].plot(xs[i][j], ys[i][j])
+            for j in range(6, 7):
+                axs[2][i].plot(xs[i][j], ys[i][j])
+    else:
+        axs[0].clear()
+        axs[1].clear()
+        axs[1].set_ylim([-500, 500])
+        axs[0].set_ylim([-5, 5])
+        # for i in range(3):
+        axs[0].plot(xs[0], ys[0])
+        # for i in range(3, 6):
+        #     axs[1].plot(xs[0][i], ys[0][i])
+        # for i in range(6, 7):
+        #     axs[2].plot(xs[0][i], ys[0][i])
+
+
+if __name__ == '__main__':
+    ani = animation.FuncAnimation(figs, update_graph, interval=1/frame_rate)
+    thread = threading.Thread(target=take_measurement)
+    thread.daemon = True
+    thread.start()
+    plt.show()
