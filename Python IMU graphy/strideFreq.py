15af9ef44f307ce7be488c2fa338d5be28435b8a
Leon228x <l.b.migaj@student.utwente.nl>
2022-06-14 19:24:46 +0200

    Added new method impact_attenuation. Wored more on fixing the code.

--- Python IMU graphy/strideFreq.py
+++ Python IMU graphy/strideFreq.py
@@ -12,6 +12,7 @@ import numpy as np
 
 from CreaTeBME import connect
 
+
 # acc: x, y, z. Gyro: x, y, z
 
 
@@ -34,12 +35,14 @@ def httpRequest(url):
     except requests.exceptions.ConnectTimeout:
         print(f"NOT CONNECT TO ESP")
 
+
 def calc_stride_freq(i):
     global filteredImpact, peaks, stride_freq
     filteredImpact = [xs[i][3].copy(), filterLow(ys[i][3])]
     deltaTime = timeImpact[-1] - timeImpact[0]
     # print(deltaTime)
     peaks = signal.find_peaks(filteredImpact[1], height=2.8, distance=25)
+
     stride_freq = (len(peaks[0])) / deltaTime * 60
     # print(f"{stride_freq = }")
 
@@ -49,38 +52,37 @@ def calc_stride_freq(i):
     else:  # then give feedback on it
         #  compare the current stride_freq with the sum(stride_freq)/len(stride_freq). als het 10% verschil heeft dan feedback.
         average_impact_calibration = np.average(np.asarray(stride_freqs))
-        print(f"{average_impact_calibration = }, {stride_freq = }, {average_impact_calibration * 0.1 = }")
+        # print(f"{average_impact_calibration = }, {stride_freq = }, {average_impact_calibration * 0.1 = }")
         if abs(average_impact_calibration - stride_freq) > average_impact_calibration * 0.1:
             url = 'smaller'
             if average_impact_calibration < stride_freq:
                 url = 'bigger'
             httpRequest(url)
-            print(f"http://192.168.4.1/{url}")
+            # print(f"http://192.168.4.1/{url}")
 
-def calc_shock_attenuation(i, measurement):
-    global peaks
 
-    array = np.array(measurement[:2])
-    normalized_vector = np.linalg.norm(array)
+def calc_shock_attenuation(leg, hip):
+    steps = 10
+    # reference = 80
+    sAtt = (1 - hip / leg) * 100
+    if sAtt > 0:
+        att_list.append(sAtt)
+    if len(att_list) > steps:
+        att_list.pop(0)
 
-    ys[i][3].append(normalized_vector)
-    xs[i][3].append(counter)
+    attAverage = 0
 
-    # print(signal.find_peaks(ys[0][1])[0])  # , height=35, distance=0.3*512))
-    impacts_foot = [ys[0][3][i] for i in peaks[0]]
-    # average_impact_foot = sum(impacts_foot) / len(impacts_foot)
-    impacts_waist = [ys[1][3][i] for i in peaks[0]]
+    for x in range(len(att_list)):
+        attAverage += att_list[x] / len(att_list)
 
-    # shock_attenuation = (1 - impacts_waist[i] / impacts_foot[i]) * 100
+    return attAverage
 
-    # average_impact_waist = sum(impacts_waist) / len(impacts_waist)
-
-    # average_impact_waist = sum(impacts_waist)/len(impacts_waist)
-
-    print(impacts_foot)
-
-    # print(f"{impacts_waist = :0.4f} m/s^2")
-    # print(shock_attenuation)
+def impact_attenuation():
+    global foot_peaks
+    foot_peaks = signal.find_peaks(ys[0][3], height=2.5)
+    for j in foot_peaks[0]:
+        print(calc_shock_attenuation(ys[0][3][j], ys[1][3][j]))
+        print(ys[0][3][j], ys[1][3][j])
 
 def take_measurement():
     old_time = time.perf_counter()
@@ -90,7 +92,6 @@ def take_measurement():
     while True:
         loop_time = time.perf_counter()
         global counter
-        peaks = [[],[]]
         for i, sensor in enumerate(sensors):
             measurement = sensor.take_measurement()
             for j, measure in enumerate(measurement[:3:]):
@@ -100,7 +101,6 @@ def take_measurement():
                     xs[i][j].pop(0)
                 if len(ys[i][j]) > limit:
                     ys[i][j].pop(0)
-
 
             timeImpact.append(time.perf_counter())
             if len(xs[i][3]) > limit:
@@ -114,20 +114,25 @@ def take_measurement():
             if old_time + 5 < time.perf_counter() and i == 0 and time.perf_counter() - startTime >= 1:
                 old_time = time.perf_counter()
                 calc_stride_freq(i)
-                # calc_shock_attenuation(i, measurement)
 
+                # for j in peaks[0][1]:
+                #     print(calc_shock_attenuation(ys[0][3][j], ys[1][3][j]))
+                #     print(ys[0][3][j], ys[1][3][j])
+                # print(peaks[1][j])
+                impact_attenuation()
+            array = np.array(measurement[:3])
+            normalized_vector = np.linalg.norm(array)
 
-
-        # print(f"total impact head = {math.sqrt(math.pow(average_impact_head_x, 2) + math.pow(average_impact_head_z, 2)):0.4f} m/s^2")
-        # print(signal.find_peaks(ys[0][1])[0])  # , height=35, distance=0.3*512))
+            ys[i][3].append(normalized_vector)
+            xs[i][3].append(counter)
+        # print(peaks[0][1])
 
         counter += 1
 
 
-
 def update_graph(counter):
     if len(sensors) > 1:
-        for i in range (len(sensors)):
+        for i in range(len(sensors)):
             axs[0][i].clear()
             axs[1][i].clear()
             axs[1][i].set_ylim([0, 10])
@@ -139,7 +144,7 @@ def update_graph(counter):
 
         axs[1][0].plot(xs[0][3], ys[0][3])
         axs[1][0].plot(filteredImpact[0], filteredImpact[1])
-
+        axs[1][1].plot(xs[1][3], ys[1][3])
         for i in peaks[0]:
             p = axs[1][0].add_patch(plt.Circle((filteredImpact[0][i], filteredImpact[1][i]), 0.5, alpha=1))
             p.set_color((0, 1, 0))
@@ -183,11 +188,12 @@ if __name__ == '__main__':
         ys.append([[] for x in range(4)])
 
     limit = 500
-    peaks = [[],[]]
+    peaks = [[], []]
+    att_list = []
     counter = 0
     stride_freq = 0
 
-    stride_freqs=[]
+    stride_freqs = []
 
     startTime = time.perf_counter()
     calibrationTime = 20  # 60*3
@@ -196,7 +202,7 @@ if __name__ == '__main__':
 
     figs, axs = plt.subplots(2, len(sensors), figsize=(10, 6))
 
-    ani = animation.FuncAnimation(figs, update_graph, interval=1/frame_rate)
+    ani = animation.FuncAnimation(figs, update_graph, interval=1 / frame_rate)
     thread = threading.Thread(target=take_measurement, daemon=True)
     thread.start()
     plt.show()
